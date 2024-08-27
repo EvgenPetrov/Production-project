d4326b21405330cef54ed1141f2b82430a019217
EvgenPetrov <bradar748@gmail.com>
2024-08-27 18:33:24 +0300

    конфиг + декомпозиция

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import webpack from "webpack";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode } = options;
+
+    return {
+        mode: "development",
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    };
+}
