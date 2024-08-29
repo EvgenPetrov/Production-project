6a02d31edc21cc56d69c2db402848b9fdec59e30
EvgenPetrov <bradar748@gmail.com>
2024-08-29 19:15:18 +0300

    Remove build and node_modules from tracking

--- build/690.26186110095e9dad831a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[690],{690:(e,n,r)=>{r.r(n),r.d(n,{MainPage:()=>i});var c=r(848),i=function(){return(0,c.jsx)("div",{children:"Main Page"})}}}]);
