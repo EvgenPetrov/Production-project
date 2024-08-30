a99017547decaf5091bd3f9ff6e40942270411ad
EvgenPetrov <bradar748@gmail.com>
2024-08-30 16:23:40 +0300

    конфиг для роутера

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,6 +1,7 @@
 // Импортируем типы для конфигурации резолвера Webpack
 import { ResolveOptions } from "webpack";
 import { BuildOptions } from "./types/config";
+import path from "path";
 
 // Функция для создания резолвера, который позволяет не указывать расширения файлов при импорте
 export function buildResolvers(options: BuildOptions): ResolveOptions {
@@ -9,6 +10,6 @@ export function buildResolvers(options: BuildOptions): ResolveOptions {
         preferAbsolute: true,
         modules: [options.paths.src, "node_modules"],
         mainFiles: ["index"],
-        alias: {},
+        alias: { "@": path.resolve(__dirname, "src") },
     };
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
+import { AppRouter } from "./providers/router";
+import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
-import { classNames } from "../shared/lib/classNames/classNames";
-import { AboutPage } from "../pages/AboutPage";
-import { MainPage } from "../pages/MainPage";
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -14,13 +11,7 @@ export const App = () => {
             <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={"/"}>Главная</Link>
             <Link to={"about"}>О сайте</Link>
-
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={"/about"} element={<AboutPage />} />
-                    <Route path={"/"} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
